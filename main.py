6dc08571772853f89ed59fd1fb416f2875d39cdb
Chiayen <932886099@qq.com>
2022-04-18 13:49:23 +0800

    4-18 提交

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 import time
+from datetime import datetime
 import re
 import os
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+
+def wait_target_loacated(target, timeout=30, poll_frequency=0.5, model=None):
+    try:
+        start = datetime.now()
+        WebDriverWait(driver, timeout, poll_frequency).until(target)
+        end = datetime.now()
+        print(f'等待"{model}"时长:{end - start}')
+    except:
+        print('等待失败，即将刷新页面')
+        raise
+
 
 options = webdriver.ChromeOptions()
 
@@ -31,7 +46,20 @@ while(current_url == login_url):
     time.sleep(1)
     current_url = driver.current_url
 
-# cookie = driver.get_cookies()
-driver.get(current_url)
-# print(cookie)
 
+driver.get(current_url)
+
+# 等待表格加载完毕
+target = EC.element_to_be_clickable((By.CLASS_NAME,'dradiostyle'))
+wait_target_loacated(target)
+cookies = driver.get_cookies()
+print(cookies)
+# 获取元素
+# mobile = driver.find_element_by_xpath('.//div[@data-key="User_11"]//input')
+# campus = driver.find_element_by_xpath('.//div[@data-key="SelectV2_58"]//input')
+# instructor = driver.find_element_by_xpath('.//div[@data-key="UserSearch_60"]//input')
+# # mentor = driver.find_element_by_xpath('.//div[@data-key="UserSearch_84""]//input')
+# mobile.send_keys('18810100529')
+# campus.send_keys('西土城校区')
+# # instructor.send_keys('王宁')
+# # mentor.send_keys('双锴')
