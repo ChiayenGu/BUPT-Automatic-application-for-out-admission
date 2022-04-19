7215b1b9471108ee083ad0b435b3843d61f69701
Chiayen <932886099@qq.com>
2022-04-19 17:32:05 +0800

    04-19 能用了

---
+++ settings.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2022 by Chiayen
+
+# 学号
+STUDENT_ID = ''
+# 密码
+PASSWORD = ''
+# 手机号
+MOBILE_NUMBER = ''
+# 辅导员姓名
+COUNSELOR_NAME = ''
+# 导师姓名
+MENTER_NAME = ''
+# 外出去向
+DESTINATION = ''
+# 外出原因
+REASON = ''
+# 出校时间 只支持整数 24小时制
+OUT_TIME = 9
+# 入校时间 只支持整数 24小时制
+BACK_TIME = 22
+
+# 星期几出入校？
+# 0=星期一 1=星期二 2=星期三 3=星期四 4=星期五 5=星期六 6=星期日
+WANT_OUT_WEEKDAY = []
